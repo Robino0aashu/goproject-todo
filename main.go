eb264ed926e11466c43a55622d10d1c1fc72397c
Robino0aashu <aashu.20404kr@gmail.com>
2025-06-18 18:04:04 +0530

    4 basic apis

--- main.go
+++ main.go
@@ -18,8 +18,54 @@ func main() {
 
 	todos:=[]Todo{}
 
-	app.Get("/", func(c *fiber.Ctx) error{
-		return c.Status(200).JSON(fiber.Map{"msg": "Hello world"})
+	app.Get("/api/todos", func(c *fiber.Ctx) error{
+		return c.Status(200).JSON(todos)
 	})
+
+	//Create a Todo
+	app.Post("api/todos", func(c *fiber.Ctx) error{
+		todo:=&Todo{} //{id:0, completed: false, body:""}
+
+		if err:=c.BodyParser(todo); err!=nil{
+			return err
+		}
+		if todo.Body==""{
+			return c.Status(400).JSON(fiber.Map{"error": "Todo body is required"})
+		}
+		todo.ID=len(todos)+1
+		todos=append(todos, *todo)
+
+		return c.Status(201).JSON(todos)
+	})
+
+
+	//update a Todo
+	app.Patch("/api/updateTodo/:id", func(c *fiber.Ctx) error{
+		id:=c.Params("id")
+		
+		for i, todo:=range todos{
+			if fmt.Sprint(todo.ID)==id{
+				todos[i].Completed = !todos[i].Completed
+				return c.Status(200).JSON(todos[i])
+			}
+		}
+		return c.Status(404).JSON(fiber.Map{
+			"error": fmt.Sprint("No todo found with this id: ", id)})
+	})
+
+	//Delete a Todo
+	app.Delete("api/deleteTodo/:id" ,func(c *fiber.Ctx) error{
+		id:=c.Params("id")
+
+		for i, todo := range todos{
+			if fmt.Sprint(todo.ID)==id{
+				todos=append(todos[:i],todos[i+1:]...)
+				return c.Status(200).JSON(todos)
+			}
+		}
+		return c.Status(404).JSON(fiber.Map{
+			"error": fmt.Sprint("No Todo found for the id: ",id)})
+	})
+
 	log.Fatal(app.Listen(":3000"))
 }
